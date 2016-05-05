fd943faf91c6a07b5f86474c82ba795704ee8471
Daniel Gamage <hello@danielgamage.com>
2016-05-05 03:03:40 -0400

    Refactoring to support multiple instances. Fixes #1

--- src/stereo-convergence.js
+++ src/stereo-convergence.js
@@ -1,73 +1,96 @@
-var playerHeight,
-	playerBox,
-	player    = document.querySelector('[data-stereoplayer]'),
-	outMin    = parseFloat(player.getAttribute("data-stereo-min")),
-	outMax    = parseFloat(player.getAttribute("data-stereo-max")),
-	eyeLeft   = player.querySelector('[data-eye="left"]'),
-	eyeRight  = player.querySelector('[data-eye="right"]'),
-	outputs   = {
-		min : ( outMin ? outMin  : -1 ),
-		max : ( outMax ? outMax : 1 )
-	},
-	clip = ( player.getAttribute("data-stereo-clip") ? player.getAttribute("data-stereo-clip") : true ),
-	inputs = {
+var Convergence = function(player){
+	var _this         = this;
+	this.player       = player;
+	this.playerHeight = 0;
+	this.playerBox    = this.player.getBoundingClientRect();
+	this.outMin       = parseFloat(this.player.getAttribute("data-stereo-min"));
+	this.outMax       = parseFloat(this.player.getAttribute("data-stereo-max"));
+	this.eyeLeft      = this.player.querySelector('[data-eye="left"]');
+	this.eyeRight     = this.player.querySelector('[data-eye="right"]');
+	this.outputs      = {
+		min : ( this.outMin ? this.outMin  : -1 ),
+		max : ( this.outMax ? this.outMax : 1 )
+	};
+	this.clip         = ( player.getAttribute("data-stereo-clip") ? player.getAttribute("data-stereo-clip") : true );
+	this.inputs       = {
 		max : 1,
 		min : 0
 	};
 
-function clipImage(){
-	var percentageMax         = Math.max( Math.abs(outputs.min), Math.abs(outputs.max) );
-	var clipMargin            = ( -1 * percentageMax ) + "%";
-	var clipWidth             = ( percentageMax * 2 + 100) + "%";
-	eyeLeft.style.maxWidth    = clipWidth;
-	eyeLeft.style.marginLeft  = clipMargin;
-	eyeRight.style.maxWidth   = clipWidth;
-	eyeRight.style.marginLeft = clipMargin;
-};
+	this.init = function () {
+		_this.bindEvents();
 
-function getDimensions(){
-	playerHeight  = player.clientHeight;
-	playerBox     = player.getBoundingClientRect();
-	playerOffset  = {
-		top: playerBox.top + document.body.scrollTop,
-		left: playerBox.left + document.body.scrollLeft
+		// Clip images if instructed
+		if ( this.clip == true ) {
+			this.clipImage();
+		}
+
+		_this.getDimensions();
+		window.onresize = _this.getDimensions();
 	};
+
+	this.bindEvents = function () {
+		// Bind events
+		this.player.addEventListener('mousemove', function (event) {
+			_this.setPositions(this, event);
+		}, false);
+		this.player.addEventListener('touchmove', function (event) {
+			// prevent scrolling
+			event.preventDefault();
+			// use single touch as event
+			_this.setPositions(this, event.touches[0]);
+		}, false);
+	};
+
+	this.clipImage = function (){
+		var percentageMax         = Math.max( Math.abs(this.outputs.min), Math.abs(this.outputs.max) );
+		var clipMargin            = ( -1 * percentageMax ) + "%";
+		var clipWidth             = ( percentageMax * 2 + 100) + "%";
+		this.eyeLeft.style.maxWidth    = clipWidth;
+		this.eyeLeft.style.marginLeft  = clipMargin;
+		this.eyeRight.style.maxWidth   = clipWidth;
+		this.eyeRight.style.marginLeft = clipMargin;
+	};
+
+	this.getDimensions = function (){
+		_this.playerHeight  = _this.player.clientHeight;
+		_this.playerBox     = _this.player.getBoundingClientRect();
+		_this.playerOffset  = {
+			top: _this.playerBox.top + document.body.scrollTop,
+			left: _this.playerBox.left + document.body.scrollLeft
+		};
+	};
+
+	this.setPositions = function (player, event){
+		if (_this.playerHeight !== player.clientHeight) {
+			_this.getDimensions();
+			return;
+		}
+		// get position
+		var mY         = event.pageY - _this.playerOffset.top;
+		// convert to percentage of image height and clip to input min / max for touch dragging
+		var yAdjusted  = Math.max( Math.min( (mY / _this.playerHeight), _this.inputs.max ), _this.inputs.min);
+		// convert to user-set range
+		var yConverted = _this.convert(yAdjusted);
+		console.log(yConverted);
+		// move eyes in opposite directions
+		_this.eyeLeft.style.transform  = "translateX(" + (-1 * yConverted) + "%)";
+		_this.eyeRight.style.transform = "translateX(" + (yConverted) + "%)";
+	};
+
+	this.convert = function (input){
+		var	inputRange  = (_this.inputs.max - _this.inputs.min);
+		var	outputRange = ( _this.outputs.max - _this.outputs.min );
+		var value       = ((input - _this.inputs.min) / (_this.inputs.max - _this.inputs.min)) * (_this.outputs.max - _this.outputs.min) + _this.outputs.min;
+		return value;
+	};
+
 };
 
-function setPositions(event){
-	if (playerHeight == player.clientHeight) {
-		getDimensions();
-		return;
-	}
-	// get position
-	var mY         = event.pageY - playerOffset.top;
-	// convert to percentage of image height and clip to input min / max for touch dragging
-	var yAdjusted  = Math.max( Math.min( (mY / playerHeight), inputs.max ), inputs.min);
-	// convert to user-set range
-	var yConverted = convert(yAdjusted);
-	// move eyes in opposite directions
-	eyeLeft.style.transform  = "translateX(" + (-1 * yConverted) + "%)";
-	eyeRight.style.transform = "translateX(" + (yConverted) + "%)";
-};
+var players   = document.querySelectorAll('[data-stereoplayer]');
+var instances = [];
+for (var i = 0; i < players.length; ++i) {
+	var player = players[i];  // Calling myNodeList.item(i) isn't necessary in JavaScript
+	instances[i] = new Convergence(player).init();
 
-function convert(input){
-	var	inputRange  = (inputs.max - inputs.min);
-	var	outputRange = ( outputs.max - outputs.min );
-	var value       = ((input - inputs.min) / (inputs.max - inputs.min)) * (outputs.max - outputs.min) + outputs.min;
-	return value;
-};
-
-getDimensions();
-window.onresize = getDimensions;
-
-if ( clip == true ) {
-	clipImage();
 }
-
-player.addEventListener('mousemove', setPositions, false);
-player.addEventListener('touchmove', function (event) {
-	// prevent scrolling
-	event.preventDefault();
-	// use single touch as event
-	setPositions(event.touches[0]);
-}, false);
